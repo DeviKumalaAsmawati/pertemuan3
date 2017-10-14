a3c68f918d8f7837ee7a6d0e1166b667a261701c
DeviKumalaAsmawati <32776989+DeviKumalaAsmawati@users.noreply.github.com>
2017-10-14 21:52:36 +0700

    ooppertemuan3

---
+++ pertemuan3/InputOutput.java
@@ -0,0 +1,11 @@
+import java.until.scanner;
+	class InputOutput{
+		public static void main (string[] args) {
+	String data;
+		Scanner scan = new Scanner (System.in);
+		System.out.ptint("Masukkan data :");
+		data = scan.nextline();
+		System.out.println ("Data yang dimasukan adalah " + data);
+	}
+
+ }
